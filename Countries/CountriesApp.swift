2b54186aeb49091536de4cba4197c68db9e132e2
Oğuzhan Kertmen <iletisim@oguzhankertmen.com.tr>
2022-10-13 13:46:38 +0300

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Oğuzhan KERTMEN on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
